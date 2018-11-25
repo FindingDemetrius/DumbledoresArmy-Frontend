09ca37ae18365d4693b62d90eaee4e7624ed5b0e
Aayush Gupta <aayush.gupta@bison.howard.edu>
2018-11-25 10:47:57 -0800

    Intermediate changes.

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -46,11 +46,3 @@ export class MapComponent implements OnInit {
     constructor(private challengeService: ChallengeService) {
     }
 }
-
-// just an interface for type safety.
-interface Marker {
-    lat: number;
-    lng: number;
-    label?: string;
-    draggable: boolean;
-}
